f9da56b5e5161b8d37da0eee7cafa5dca3dfff15
MohamedSalmankhan <45882892+MohamedSalmankhan@users.noreply.github.com>
2019-01-03 01:48:28 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,10 +4,8 @@ variable "instance_ami" {
 variable "key_name" {
 }
 variable "subnet_id" {
-  default = "subnet-3345c01d"
 }
 variable "sg_id" {
-  default = "sg-022d9fafc38d5b7db"
 }
 variable "db_name" {
 }
@@ -16,11 +14,8 @@ variable "username" {
 variable "password" {
 }
 variable "db_subnet-1" {
-  default = "subnet-608bc52a"
 }
 variable "db_subnet-2" {
-  default = "subnet-738bf87c"
 }
 variable "db_sg" {
-  default = "sg-0002f611426c2b3a6"
 }
